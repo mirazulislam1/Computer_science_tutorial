b06ea2ba178218189d3db337c9809f7e635a9397
unknown <you@example.com>
2022-10-25 19:29:26 +0600

    login form added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,30 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const Login = () => {
     return (
-        <div>
-            <h1>login</h1>
-        </div>
+        <Form className='w-50 mx-auto mt-4'>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control name='email' type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control name='password' type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            <Form.Text className="text-muted">
+
+            </Form.Text>
+        </Form>
     );
 };
 
